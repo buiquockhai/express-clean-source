5b2bdfd4f886e9149673353a1e08e35c56e65ba8
khai.bui@droppii.com <khai.bui@droppii.com>
2022-11-10 19:39:51 +0700

    fix: db connection information

--- src/util/constraints.js
+++ src/util/constraints.js
@@ -3,10 +3,10 @@ const HOST = "0.0.0.0";
 const PORT = 8000;
 
 const PG_CONFIG = {
-  username: process.env.PG_USERNAME || "postgres",
-  password: process.env.PG_PASSWORD || "123456",
-  database: process.env.PG_DATABASE || "LearnAndDo",
-  host: process.env.PG_HOST || "localhost",
+  username: process.env.PG_USERNAME || "librate",
+  password: process.env.PG_PASSWORD || "Librate2022",
+  database: process.env.PG_DATABASE || "literate",
+  host: process.env.PG_HOST || "literate.ci3ljp90yw4z.ap-southeast-1.rds.amazonaws.com",
   port: process.env.PG_PORT || "5432",
   dialect: "postgres",
   logging: false,
